23f1ad9d8582f77e85e264d9fe23316a47cbb13c
DEV <manik@Manikanta>
2020-08-03 14:46:37 +0530

    basic directory structure created

---
+++ StudentApplicationStatus/src/main/java/com/mnt/StudentApplicationStatusApplication.java
@@ -0,0 +1,13 @@
+package com.mnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplicationStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplicationStatusApplication.class, args);
+	}
+
+}
